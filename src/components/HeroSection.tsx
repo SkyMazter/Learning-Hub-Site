3fd8a7a4e4310c387d8544fc1c240501d5267301
SkyMazter <comunidadoscar@gmail.com>
2025-05-23 15:30:21 -0400

    added mission

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,9 +1,19 @@
- const HeroSection: React.FC = () => {
+const HeroSection: React.FC = () => {
   return (
     <section className="hero-section">
       <div className="container hero-content">
-        <h1>Master New Skills, <br />One Module at a Time</h1>
-        <p>Start from zero. Learn Docker, Blender, GiS, Git, Linux, and more—all in one place.</p>
+        <h1>
+          Master New Skills, <br />
+          One Module at a Time
+        </h1>
+        <p>
+          This evolving platform is dedicated to empowering new developers by
+          illuminating the essential concepts and practical skills rarely taught
+          in conventional coding classes and bootcamps. My mission is to share
+          distilled, real-world knowledge and personal learnings, ensuring that
+          aspiring programmers gain a holistic understanding of the development
+          landscape from day one.
+        </p>
         <button className="btn primary-btn">Explore Subjects</button>
       </div>
     </section>
